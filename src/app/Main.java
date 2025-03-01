53ef91fbac87072f93b1e3f33052ade061e7de89
melmanss <cfhfyjdbx@gmail.com>
2025-03-01 11:17:41 +0200

    New commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+package app;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+
+    public static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App. ");
+
+        double kgs = 5;
+        double pounds = convKgsToPounds (kgs);
+        System.out.println("Result is " + pounds + " pounds");
+    }
+    public static double convKgsToPounds (double kgs) {
+        return kgs * CONV_K;
     }
 }
